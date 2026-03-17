5bcaa562babf27584044d753f5eb966b08080341
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-17 18:24:28 +0530

    edit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://scamshield-1.onrender.com',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // env load karega (.env, .env.development, etc.)
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // env se URL lega
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
+  }
 })
